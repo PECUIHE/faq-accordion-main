07e3680f41e0724262066828d4f599b2566dc3d3
peculiar <pecuiheubammadu1st@gmail.com>
2024-09-04 02:09:11 +0100

    Commit

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const accordionBtns = document.querySelectorAll('.accordion-btn');
 accordionBtns.forEach(btn => {
     btn.addEventListener('click', function () {
         this.classList.toggle('active')
-        const accordionDescription = this.nextElementSibling
+        const accordionAnswer = this.nextElementSibling
         const plusIcon = this.querySelector('.plus-icon')
         const minusIcon = this.querySelector('.minus-icon')
 
-        if(accordionDescription.style.maxHeight) {
-            accordionDescription.style.maxHeight = null
+        if(accordionAnswer.style.maxHeight) {
+            accordionAnswer.style.maxHeight = null
             plusIcon.style.display = 'block'
             minusIcon.style.display = 'none'
         } else {
-            accordionDescription.style.maxHeight = accordionDescription.scrollHeight + 'px'
+            accordionAnswer.style.maxHeight = accordionAnswer.scrollHeight + 'px'
             plusIcon.style.display = 'none'
             minusIcon.style.display = 'block'
         }
